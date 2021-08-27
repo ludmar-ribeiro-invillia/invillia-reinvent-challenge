9cc1a3e82d626362411a48411ba930b3363d3971
Thandravale Silva <thandravale@gmail.com>
2021-08-27 19:53:30 -0300

    add search by product sku and customer id on CartRepository

--- src/main/java/com/invillia/challenge/shoppingcart/repository/CartRepository.java
+++ src/main/java/com/invillia/challenge/shoppingcart/repository/CartRepository.java
@@ -4,4 +4,5 @@ import com.invillia.challenge.shoppingcart.entity.Cart;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CartRepository extends CrudRepository<Cart, Integer> {
+    Cart findByProduct_SkuAndCustomer_Id(String sku,Integer id);
 }
