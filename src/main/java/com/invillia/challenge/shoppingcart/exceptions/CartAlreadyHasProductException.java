2afdf6f3f0e5bb543d8cdaf74b59d7fb92f03f19
Thandravale Silva <thandravale@gmail.com>
2021-08-29 19:23:41 -0300

    add CartAlreadyHasProductException

---
+++ src/main/java/com/invillia/challenge/shoppingcart/exceptions/CartAlreadyHasProductException.java
@@ -0,0 +1,20 @@
+package com.invillia.challenge.shoppingcart.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class CartAlreadyHasProductException extends RuntimeException {
+    protected final String RESOURCE= "Cart";
+    protected final String ERROR_KEY= "cart already have a product with this sku";
+    private final String resourceKey;
+
+    public CartAlreadyHasProductException(String message, String resourceKey) {
+        super(message);
+        this.resourceKey = resourceKey;
+    }
+
+    public String getResourceKey() {
+        return resourceKey;
+    }
+}
