e6622d425f940fcf696eb5db183c7facff80eceb
Thandravale Silva <thandravale@gmail.com>
2021-08-29 18:37:28 -0300

    add CustomerNotFoundException

---
+++ src/main/java/com/invillia/challenge/shoppingcart/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,20 @@
+package com.invillia.challenge.shoppingcart.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class CustomerNotFoundException extends RuntimeException {
+    protected final String RESOURCE= "customer";
+    protected final String ERROR_KEY= "customer was not found";
+    private final String resourceKey;
+
+    public CustomerNotFoundException(String message, String resourceKey) {
+        super(message);
+        this.resourceKey = resourceKey;
+    }
+
+    public String getResourceKey() {
+        return resourceKey;
+    }
+}
