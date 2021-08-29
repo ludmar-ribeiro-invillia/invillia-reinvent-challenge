5e980a9a13dbd3f64520decbbad1111ce1d2e3ae
Thandravale Silva <thandravale@gmail.com>
2021-08-29 19:48:13 -0300

    add CartNotFound handler

--- src/main/java/com/invillia/challenge/shoppingcart/exceptions/ResponseHandler.java
+++ src/main/java/com/invillia/challenge/shoppingcart/exceptions/ResponseHandler.java
@@ -53,4 +53,15 @@ public class ResponseHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, errorResponse,
                 new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
+
+    @ExceptionHandler(value = {CartNotFoundException.class})
+    protected ResponseEntity<Object> CartNotFoundHandler(CartAlreadyHasProductException ex, WebRequest request){
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setMessage(ex.getMessage());
+        errorResponse.setError_key(ex.ERROR_KEY);
+        errorResponse.setResource(ex.RESOURCE);
+        errorResponse.setResource_key(ex.getResourceKey());
+        return handleExceptionInternal(ex, errorResponse,
+                new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
 }
