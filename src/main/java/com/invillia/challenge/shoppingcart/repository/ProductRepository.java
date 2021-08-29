e26b4ae42c124b37735b80b478496c15c012f0ae
Thandravale Silva <thandravale@gmail.com>
2021-08-29 18:35:36 -0300

    add findBySku method on ProductRepository

--- src/main/java/com/invillia/challenge/shoppingcart/repository/ProductRepository.java
+++ src/main/java/com/invillia/challenge/shoppingcart/repository/ProductRepository.java
@@ -4,4 +4,5 @@ import com.invillia.challenge.shoppingcart.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product, String> {
+    Product findBySku(String sku);
 }
