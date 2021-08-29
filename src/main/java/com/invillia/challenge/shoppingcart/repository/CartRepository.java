796e6bab063fac08d1715641579b6c879fdc18ba
Thandravale Silva <thandravale@gmail.com>
2021-08-29 16:49:57 -0300

    Add findByCustomer_Id and deleteByCustomer_id method to CartRepository

--- src/main/java/com/invillia/challenge/shoppingcart/repository/CartRepository.java
+++ src/main/java/com/invillia/challenge/shoppingcart/repository/CartRepository.java
@@ -2,7 +2,13 @@ package com.invillia.challenge.shoppingcart.repository;
 
 import com.invillia.challenge.shoppingcart.entity.Cart;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface CartRepository extends CrudRepository<Cart, Integer> {
     Cart findByProduct_SkuAndCustomer_Id(String sku,Integer id);
+    List<Cart> findByCustomer_Id(Integer id);
+    @Transactional
+    void deleteByCustomer_Id(Integer id);
 }
