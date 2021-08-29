dff132f7741189175d0b3e9a404916833284fdf9
Thandravale Silva <thandravale@gmail.com>
2021-08-29 18:40:37 -0300

    add ResponseHandler to handle the exceptions thrown in ShoppingCartService

---
+++ src/main/java/com/invillia/challenge/shoppingcart/exceptions/ResponseHandler.java
@@ -0,0 +1,23 @@
+package com.invillia.challenge.shoppingcart.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ResponseHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = {CustomerNotFoundException.class})
+    protected ResponseEntity<Object> CustomerNotFoundHandler(CustomerNotFoundException ex, WebRequest request){
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setMessage(ex.getMessage());
+        errorResponse.setError_key(ex.ERROR_KEY);
+        errorResponse.setResource(ex.RESOURCE);
+        errorResponse.setResource_key(ex.getResourceKey());
+        return handleExceptionInternal(ex, errorResponse,
+                new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+}
